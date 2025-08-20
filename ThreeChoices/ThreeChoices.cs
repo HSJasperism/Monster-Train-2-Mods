daef8894fc3ea65b863b5de4d4974fbf862d0d40
HSJasperism <47498453+HSJasperism@users.noreply.github.com>
2025-08-20 23:48:56 +0800

    Add project files.

---
+++ ThreeChoices/ThreeChoices.cs
@@ -0,0 +1,70 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ThreeChoices
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess("MonsterTrain2.exe")]
+    public class ThreeChoices : BaseUnityPlugin
+    {
+        public const string GUID = "com.mod.threechoices";
+        public const string NAME = "Three Choices";
+        public const string VERSION = "0.0.1";
+
+        public static uint changeFrom = 2u;
+        public static uint changeTo = 3u;
+
+        public static readonly FieldInfo RelicTool = AccessTools.Field(typeof(RelicDraftRewardData), "draftOptionsCount");
+        public static readonly FieldInfo DraftTool = AccessTools.Field(typeof(DraftRewardData), "draftOptionsCount");
+
+        public static void UpdateHerzalHoard(RelicDraftRewardData target)
+        {
+            if ((uint)RelicTool.GetValue(target) == changeFrom)
+            {
+                RelicTool.SetValue(target, changeTo);
+            }
+        }
+
+        public static void UpdateBanner(DraftRewardData target)
+        {
+            if ((uint)DraftTool.GetValue(target) == changeFrom)
+            {
+                DraftTool.SetValue(target, changeTo);
+            }
+        }
+
+        public void Awake()
+        {
+            Harmony harmony = new Harmony(GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(RelicDraftRewardData), "GatherPregeneratedRelics")]
+    internal class RelicDraftRewardData_GatherPregeneratedRelics_Patch
+    {
+        internal static void Prefix(RelicDraftRewardData __instance)
+        {
+            ThreeChoices.UpdateHerzalHoard(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(RelicDraftRewardData), "GetRelicsFromClasses")]
+    internal class RelicDraftRewardData_GetRelicsFromClasses_Patch
+    {
+        internal static void Prefix(RelicDraftRewardData __instance)
+        {
+            ThreeChoices.UpdateHerzalHoard(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(DraftRewardData), "GetCardsFromClasses")]
+    internal class DraftRewardData_GetCardsFromClasses_Patch
+    {
+        internal static void Prefix(DraftRewardData __instance)
+        {
+            ThreeChoices.UpdateBanner(__instance);
+        }
+    }
+}
