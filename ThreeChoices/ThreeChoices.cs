e0c542d62e6057e3be8e5c4fcef3d092e21a108b
HSJasperism <47498453+HSJasperism@users.noreply.github.com>
2025-10-04 21:59:25 +0800

    Refactor

--- ThreeChoices/ThreeChoices.cs
+++ ThreeChoices/ThreeChoices.cs
@@ -12,28 +12,6 @@ namespace ThreeChoices
         public const string NAME = "Three Choices";
         public const string VERSION = "0.0.1";
 
-        public static uint changeFrom = 2u;
-        public static uint changeTo = 3u;
-
-        private static readonly FieldInfo RelicTool = AccessTools.Field(typeof(RelicDraftRewardData), "draftOptionsCount");
-        private static readonly FieldInfo DraftTool = AccessTools.Field(typeof(DraftRewardData), "draftOptionsCount");
-
-        public static void UpdateHerzalHoard(RelicDraftRewardData target)
-        {
-            if ((uint)RelicTool.GetValue(target) == changeFrom)
-            {
-                RelicTool.SetValue(target, changeTo);
-            }
-        }
-
-        public static void UpdateBanner(DraftRewardData target)
-        {
-            if ((uint)DraftTool.GetValue(target) == changeFrom)
-            {
-                DraftTool.SetValue(target, changeTo);
-            }
-        }
-
         public void Awake()
         {
             var Hook = new Harmony(GUID);
@@ -41,21 +19,46 @@ namespace ThreeChoices
         }
     }
 
+    public static class ThreeChoicesHelpers
+    {
+        private static readonly FieldInfo RelicTool = AccessTools.Field(typeof(RelicDraftRewardData), "draftOptionsCount");
+        private static readonly FieldInfo DraftTool = AccessTools.Field(typeof(DraftRewardData), "draftOptionsCount");
+
+        public static uint changeFrom = 2u;
+        public static uint changeTo = 3u;
+
+        public static void UpdateBanner(this DraftRewardData target)
+        {
+            if ((uint)DraftTool.GetValue(target) == changeFrom)
+            {
+                DraftTool.SetValue(target, changeTo);
+            }
+        }
+
+        public static void UpdateHerzalHoard(this RelicDraftRewardData target)
+        {
+            if ((uint)RelicTool.GetValue(target) == changeFrom)
+            {
+                RelicTool.SetValue(target, changeTo);
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(RelicDraftRewardData), "GatherPregeneratedRelics")]
     internal class RelicDraftRewardData_GatherPregeneratedRelics_Patch
     {
-        internal static void Prefix(RelicDraftRewardData __instance) => ThreeChoices.UpdateHerzalHoard(__instance);
+        internal static void Prefix(RelicDraftRewardData __instance) => __instance.UpdateHerzalHoard();
     }
 
     [HarmonyPatch(typeof(RelicDraftRewardData), "GetRelicsFromClasses")]
     internal class RelicDraftRewardData_GetRelicsFromClasses_Patch
     {
-        internal static void Prefix(RelicDraftRewardData __instance) => ThreeChoices.UpdateHerzalHoard(__instance);
+        internal static void Prefix(RelicDraftRewardData __instance) => __instance.UpdateHerzalHoard();
     }
 
     [HarmonyPatch(typeof(DraftRewardData), "GetCardsFromClasses")]
     internal class DraftRewardData_GetCardsFromClasses_Patch
     {
-        internal static void Prefix(DraftRewardData __instance) => ThreeChoices.UpdateBanner(__instance);
+        internal static void Prefix(DraftRewardData __instance) => __instance.UpdateBanner();
     }
 }
